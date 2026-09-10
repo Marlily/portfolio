6cc6f7d8dc35410760f0235d04bcd945db8ff727
Martyna Banaś <banas.martynaa@gmail.com>
2026-09-10 14:04:26 +0200

    CHANGE: projects names

--- portfolio2/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require get_template_directory() . '/inc/setup.php';
-require get_template_directory() . '/inc/enqueue.php';
-require get_template_directory() . '/inc/acf.php';
-require get_template_directory() . '/inc/menu.php';
-require get_template_directory() . '/inc/i18n.php';
